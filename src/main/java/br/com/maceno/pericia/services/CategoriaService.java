092d6d98b0a9355f1ba713185aca80f433b80d32
Sandro Maceno <smaceno@gmail.com>
2020-05-12 21:22:29 -0300

    Update CategoriaService.java

--- src/main/java/br/com/maceno/pericia/services/CategoriaService.java
+++ src/main/java/br/com/maceno/pericia/services/CategoriaService.java
@@ -17,7 +17,7 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = categoriaRepository.findById(id);
-		return obj.orElseThrow(() -> new ObjectNotFoundException("ops, busca não encontrada! Id: " + id
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Ops, busca não encontrada! Id: " + id
 			+ " Tipo: " + Categoria.class.getName()));
 	}
 }

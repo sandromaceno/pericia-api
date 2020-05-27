8dce0e13666315766fc62a6979fff7d819347503
Sandro Maceno <smaceno@gmail.com>
2020-05-26 22:34:45 -0300

    Implementação inicial da inclusão e busca do acidente

--- src/main/java/br/com/maceno/pericia/PericiaApiApplication.java
+++ src/main/java/br/com/maceno/pericia/PericiaApiApplication.java
@@ -1,12 +1,17 @@
 package br.com.maceno.pericia;
 
+import java.time.LocalDateTime;
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.maceno.pericia.entity.Acidente;
 import br.com.maceno.pericia.entity.Usuario;
 import br.com.maceno.pericia.enums.TipoAcessoEnum;
+import br.com.maceno.pericia.repository.AcidenteRepository;
 import br.com.maceno.pericia.repository.UsuarioRepository;
 import br.com.maceno.pericia.services.Pessoa;
 import br.com.maceno.pericia.services.PessoaServiceFactory;
@@ -16,6 +21,9 @@ public class PericiaApiApplication implements CommandLineRunner{
 
 	@Autowired
 	private UsuarioRepository usuarioRepository;
+
+	@Autowired
+	private AcidenteRepository acidenteRepository;
 
 	public static void main(String[] args) {
 		
@@ -28,12 +36,13 @@ public class PericiaApiApplication implements CommandLineRunner{
         pessoa.tipoAcesso = TipoAcessoEnum.POLICIAL;
         factory.getPessoa(pessoa);
         
-		//SpringApplication.run(PericiaApiApplication.class, args);
+		SpringApplication.run(PericiaApiApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
 		
+		//USUARIOS
 		Usuario usu1 = new Usuario(null, "Sandro", "smaceno@gmail.com", "123", "996767030", TipoAcessoEnum.POLICIAL);
 		Usuario usu2 = new Usuario(null, "Luis", "luis@gmail.com", "123", "9685741414", TipoAcessoEnum.CIVIL);
 		Usuario usu3 = new Usuario(null, "Gustavo", "gustavo@gmail.com", "123", "3333741414", TipoAcessoEnum.CIVIL);
@@ -42,8 +51,13 @@ public class PericiaApiApplication implements CommandLineRunner{
 		Usuario usu6 = new Usuario(null, "Gisele", "gisele@gmail.com", "123", "6666741414", TipoAcessoEnum.CIVIL);
 		Usuario usu7 = new Usuario(null, "Andressa", "andressa@gmail.com", "123", "7777741414", TipoAcessoEnum.CIVIL);
 		Usuario usu8 = new Usuario(null, "Andrielle", "andrielle@gmail.com", "123", "8888741414", TipoAcessoEnum.CIVIL);
-		
 		//usuarioRepository.saveAll(Arrays.asList(usu1, usu2, usu3, usu4, usu5, usu6, usu7, usu8));
+		
+		// ACIDENTES
+		LocalDateTime dataTime = LocalDateTime.now();
+		Acidente acid1 = new Acidente(null, -25.3763177, -49.227953, dataTime.toLocalDate(), dataTime.toLocalTime());
+		Acidente acid2 = new Acidente(null, -25.464463, -49.1989722, dataTime.toLocalDate(), dataTime.toLocalTime());
+		//acidenteRepository.saveAll(Arrays.asList(acid1, acid2));
 		
 	}
 	
---
+++ src/main/java/br/com/maceno/pericia/controller/AcidenteController.java
@@ -0,0 +1,39 @@
+package br.com.maceno.pericia.controller;
+
+import java.net.URI;
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import br.com.maceno.pericia.entity.Acidente;
+import br.com.maceno.pericia.services.AcidenteService;
+
+@RestController
+@RequestMapping(value="/acidentes")
+public class AcidenteController {
+	
+	@Autowired
+	private AcidenteService acidenteService;
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<Acidente>> buscarTodos() {
+		List<Acidente> acidente = acidenteService.buscarTodos();
+		return ResponseEntity.ok().body(acidente);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody Acidente acidente) {
+		acidente = acidenteService.inserir(acidente);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(acidente.getMarkerId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+
+}

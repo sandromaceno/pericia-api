f582b092dfe15460b040a1c156fd20692181bfb4
Sandro Maceno <smaceno@gmail.com>
2020-05-29 00:30:22 -0300

    implementação da exclusão

--- src/main/java/br/com/maceno/pericia/controller/AcidenteController.java
+++ src/main/java/br/com/maceno/pericia/controller/AcidenteController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -34,6 +35,12 @@ public class AcidenteController {
 		acidente = acidenteService.inserir(acidente);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(acidente.getMarkerId()).toUri();
 		return ResponseEntity.created(uri).build();
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> deletar(@PathVariable Integer id) {
+		acidenteService.deletar(id);
+		return ResponseEntity.noContent().build();
 	}
 
 }
--- src/main/java/br/com/maceno/pericia/services/AcidenteService.java
+++ src/main/java/br/com/maceno/pericia/services/AcidenteService.java
@@ -1,12 +1,15 @@
 package br.com.maceno.pericia.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.maceno.pericia.constants.MensagemConstants;
 import br.com.maceno.pericia.entity.Acidente;
 import br.com.maceno.pericia.repository.AcidenteRepository;
+import br.com.maceno.pericia.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class AcidenteService {
@@ -18,10 +21,19 @@ public class AcidenteService {
 		return acidenteRepository.findAll();
 	}
 	
+	public Acidente buscar(Integer id) {
+		Optional<Acidente> acidente = acidenteRepository.findById(id);
+		return acidente.orElseThrow(() -> new ObjectNotFoundException(MensagemConstants.BUSCA_NAO_ENCONTRADA + "! Id: " + id));
+	}
 	
 	public Acidente inserir(Acidente acidente) {
 		acidente.setMarkerId(null);
 		return acidenteRepository.save(acidente);
+	}
+	
+	public void deletar(Integer id) {
+		buscar(id);
+		acidenteRepository.deleteById(id);
 	}
 
 	
